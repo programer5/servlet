81d280a16735caf84d9e01767cf6ae476e7908de
programer5 <neverdie4757@gmail.com>
2021-10-27 22:53:15 +0900

    핸들러 매핑과 핸들러 어댑터

--- src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -1,22 +1,17 @@
 package hello.servlet.web.springmvc.old;
 
-import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.HttpRequestHandler;
+import java.io.IOException;
 
 @Component("/springmvc/request-handler")
 public class MyHttpRequestHandler implements HttpRequestHandler {
-
     @Override
-    public void handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-                System.out.println("MyHttpRequestHandler.handleRequest()");
-        
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
     }
-    
 }
